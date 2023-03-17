977bc8d98bb984578d66dbee2e74cb665a011b38
Bobrikov <bobronaud@gmail.com>
2023-03-17 12:43:48 +0300

    rewrite tests for nested objects, add nested fixtures and remove flat fixtures

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -10,8 +10,7 @@ const __dirname = dirname(__filename);
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 const readFile = (filename) => readFileSync(getFixturePath(filename), 'utf-8');
 
-test('flat objects', () => {
-  expect(genDiff(getFixturePath('file1.json'), getFixturePath('file2.json'))).toEqual(readFile('genDiffResult.txt'));
-  expect(genDiff(getFixturePath('file1.yml'), getFixturePath('file2.yml'))).toEqual(readFile('genDiffResult.txt'));
-  expect(genDiff(getFixturePath('file1.yaml'), getFixturePath('file2.yaml'))).toEqual(readFile('genDiffResult.txt'));
+test('nested objects', () => {
+  expect(genDiff(getFixturePath('file1-nested.json'), getFixturePath('file2-nested.json'))).toEqual(readFile('genDiffResult-nesten.txt'));
+  expect(genDiff(getFixturePath('file1-nested.yml'), getFixturePath('file2-nested.yml'))).toEqual(readFile('genDiffResult-nesten.txt'));
 });
