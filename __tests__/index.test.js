d06ab2234e85360b34d6abfadd435556a6838427
Bobrikov <bobronaud@gmail.com>
2023-03-20 20:20:14 +0300

    rewriting nested tests

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -2,7 +2,8 @@ import { fileURLToPath } from 'url';
 import { dirname } from 'path';
 import path from 'node:path';
 import { readFileSync } from 'node:fs';
-import genDiff from '../src/index';
+import genDiff from '../src/index.js';
+import stylish from '../src/stylish.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -10,7 +11,11 @@ const __dirname = dirname(__filename);
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 const readFile = (filename) => readFileSync(getFixturePath(filename), 'utf-8');
 
+const diffJson = genDiff(getFixturePath('file1-nested.json'), getFixturePath('file2-nested.json'));
+const diffYaml = genDiff(getFixturePath('file1-nested.yml'), getFixturePath('file2-nested.yml'));
+const expected = readFile('genDiffResult-nested.txt');
+
 test('nested objects', () => {
-  expect(genDiff(getFixturePath('file1-nested.json'), getFixturePath('file2-nested.json'))).toEqual(readFile('genDiffResult-nesten.txt'));
-  expect(genDiff(getFixturePath('file1-nested.yml'), getFixturePath('file2-nested.yml'))).toEqual(readFile('genDiffResult-nesten.txt'));
+  expect(stylish(diffJson)).toEqual(expected);
+  expect(stylish(diffYaml)).toEqual(expected);
 });
