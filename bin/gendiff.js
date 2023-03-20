a5663812dc3a11c124010c22b3c1a67e9608fffa
Bobrikov <bobronaud@gmail.com>
2023-03-20 20:20:58 +0300

    add stylish format

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -2,6 +2,7 @@
 
 import { Command } from 'commander';
 import genDiff from '../src/index.js';
+import stylish from '../src/stylish.js';
 
 const program = new Command();
 
@@ -9,8 +10,11 @@ program
   .arguments('<filepath1> <filepath2>')
   .description('Compares two configuration files and shows a difference.')
   .version('0.1.0')
-  .option('-f, --format <type>', 'output format')
+  .option('-f, --format <type>', 'output format', 'stylish')
   .action((filepath1, filepath2) => {
-    console.log(genDiff(filepath1, filepath2));
+    const diff = genDiff(filepath1, filepath2);
+    if (program.opts().format === undefined || program.opts().format === 'stylish') {
+      console.log(stylish(diff));
+    }
   });
 program.parse();
--- src/index.js
+++ src/index.js
@@ -7,30 +7,70 @@ const isValidExtension = (filepath) => {
   return validExtensions.includes(path.extname(filepath));
 };
 
+const buildValue = (value) => {
+  if (!_.isObject(value)) {
+    return value;
+  }
+  const entries = Object.entries(value);
+  const result = entries.reduce((acc, [key, newValue]) => {
+    const node = {};
+    acc.push(node);
+    node.key = key;
+    node.value = _.isObject(newValue) ? buildValue(newValue) : newValue;
+    return acc;
+  }, []);
+  return result;
+};
+
 const genDiff = (filepath1, filepath2) => {
   if (!isValidExtension(filepath1) || !isValidExtension(filepath2)) {
     return 'Error: file is not valid exnatsion';
   }
-  const file1 = parse(filepath1);
-  const file2 = parse(filepath2);
-  const sortedAllKeys = _.union(_.keys(file1), _.keys(file2)).sort();
-  const result = sortedAllKeys.reduce((diff, key) => {
-    if (!Object.hasOwn(file1, key)) {
-      diff = `${diff}\n  + ${key}: ${file2[key]}`;
+  const firstFile = parse(filepath1);
+  const secondFile = parse(filepath2);
+
+  const iter = (file1, file2) => {
+    const sortedAllKeys = _.union(_.keys(file1), _.keys(file2)).sort();
+    const result = sortedAllKeys.reduce((diff, key) => {
+      const value1 = file1[key];
+      const value2 = file2[key];
+      const node = {};
+      diff.push(node);
+      if (_.isObject(value1) && _.isObject(value2)) {
+        node.key = key;
+        node.status = 'unchanged';
+        node.value = iter(value1, value2);
+        return diff;
+      }
+      if (!Object.hasOwn(file1, key)) {
+        node.key = key;
+        node.status = 'added';
+        node.value = buildValue(value2);
+        return diff;
+      }
+      if (!Object.hasOwn(file2, key)) {
+        node.key = key;
+        node.status = 'removed';
+        node.value = buildValue(value1);
+        return diff;
+      }
+      if (value1 !== value2) {
+        node.key = key;
+        node.status = 'changed';
+        const oldValue = buildValue(value1);
+        const newValue = buildValue(value2);
+        node.value = { oldValue, newValue };
+        return diff;
+      }
+      node.key = key;
+      node.status = 'unchanged';
+      node.value = buildValue(value1);
       return diff;
-    }
-    if (!Object.hasOwn(file2, key)) {
-      diff = `${diff}\n  - ${key}: ${file1[key]}`;
-      return diff;
-    }
-    if (file1[key] !== file2[key]) {
-      diff = `${diff}\n  - ${key}: ${file1[key]}\n  + ${key}: ${file2[key]}`;
-      return diff;
-    }
-    diff = `${diff}\n    ${key}: ${file1[key]}`;
-    return diff;
-  }, '');
-  return `{${result}\n}`;
+    }, []);
+    return result;
+  };
+  return iter(firstFile, secondFile);
+  // console.dir(iter(firstFile, secondFile), { depth: null });
 };
 
 export default genDiff;
