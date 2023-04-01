ca9d8a45a630d71f5b0a9a4944416af4397a0f76
Bobrikov <bobronaud@gmail.com>
2023-04-01 13:02:40 +0300

    remove parsing function from buildDiffTree.js to index.js

--- src/buildDiffTree.js
+++ src/buildDiffTree.js
@@ -1,8 +1,4 @@
 import _ from 'lodash';
-import path from 'node:path';
-import parse from './parsers.js';
-
-const buildObject = (filepath) => parse(filepath, path.extname(filepath));
 
 const buildValue = (buildedValue) => {
   if (!_.isObject(buildedValue)) {
@@ -16,40 +12,35 @@ const buildValue = (buildedValue) => {
   return result;
 };
 
-const buildDiffTree = (filepath1, filepath2) => {
-  const firstObj = buildObject(filepath1);
-  const secondObj = buildObject(filepath2);
-  const iter = (obj1, obj2) => {
-    const sortedAllKeys = _.sortBy(_.union(_.keys(obj1), _.keys(obj2)));
-    return sortedAllKeys.reduce((diff, key) => {
-      const value1 = obj1[key];
-      const value2 = obj2[key];
-      if (_.isObject(value1) && _.isObject(value2)) {
-        return [...diff, { key, status: 'nested', value: iter(value1, value2) }];
-      }
-      if (!Object.hasOwn(obj1, key)) {
-        return [...diff, { key, status: 'added', value: buildValue(value2) }];
-      }
-      if (!Object.hasOwn(obj2, key)) {
-        return [...diff, { key, status: 'removed', value: buildValue(value1) }];
-      }
-      if (value1 !== value2) {
-        return [
-          ...diff,
-          {
-            key,
-            status: 'changed',
-            value: {
-              oldValue: buildValue(value1),
-              newValue: buildValue(value2),
-            },
+const buildDiffTree = (obj1, obj2) => {
+  const sortedAllKeys = _.sortBy(_.union(_.keys(obj1), _.keys(obj2)));
+  return sortedAllKeys.reduce((diff, key) => {
+    const value1 = obj1[key];
+    const value2 = obj2[key];
+    if (_.isObject(value1) && _.isObject(value2)) {
+      return [...diff, { key, status: 'nested', value: buildDiffTree(value1, value2) }];
+    }
+    if (!Object.hasOwn(obj1, key)) {
+      return [...diff, { key, status: 'added', value: buildValue(value2) }];
+    }
+    if (!Object.hasOwn(obj2, key)) {
+      return [...diff, { key, status: 'removed', value: buildValue(value1) }];
+    }
+    if (value1 !== value2) {
+      return [
+        ...diff,
+        {
+          key,
+          status: 'changed',
+          value: {
+            oldValue: buildValue(value1),
+            newValue: buildValue(value2),
           },
-        ];
-      }
-      return [...diff, { key, status: 'unchanged', value: buildValue(value1) }];
-    }, []);
-  };
-  return iter(firstObj, secondObj);
+        },
+      ];
+    }
+    return [...diff, { key, status: 'unchanged', value: buildValue(value1) }];
+  }, []);
 };
 
 export default buildDiffTree;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
+import path from 'node:path';
 import formatting from './formatters/index.js';
 import buildDiffTree from './buildDiffTree.js';
+import parse from './parsers.js';
 
 const genDiff = (filepath1, filepath2, formatName = 'stylish') => {
-  const tree = buildDiffTree(filepath1, filepath2);
+  const file1 = parse(filepath1, path.extname(filepath1));
+  const file2 = parse(filepath2, path.extname(filepath2));
+  const tree = buildDiffTree(file1, file2);
   return formatting(tree, formatName);
 };
 
