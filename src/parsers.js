a0904b6f0dd619417778d395f19088da461838d5
Bobrikov <bobronaud@gmail.com>
2023-04-02 13:25:43 +0300

    remove reading files from module. change param extname to type

--- src/parsers.js
+++ src/parsers.js
@@ -1,20 +1,16 @@
-import { readFileSync } from 'node:fs';
-import path from 'node:path';
 import yaml from 'js-yaml';
 
-const parse = (filepath, extname) => {
-  switch (extname) {
-    case '.json':
-      return JSON.parse(readFileSync(path.resolve(filepath), 'utf-8'));
+const parse = (data, type) => {
+  switch (type) {
+    case 'json':
+      return JSON.parse(data);
 
-    case '.yml':
-      return yaml.load(readFileSync(path.resolve(filepath), 'utf-8'));
-
-    case '.yaml':
-      return yaml.load(readFileSync(path.resolve(filepath), 'utf-8'));
+    case 'yml':
+    case 'yaml':
+      return yaml.load(data);
 
     default:
-      throw new Error('file is not valid exnatsion');
+      throw new Error('data is not valid type');
   }
 };
 
