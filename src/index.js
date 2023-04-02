476f2e093a09b7845a1b6e7ff294bba2094f682e
Bobrikov <bobronaud@gmail.com>
2023-04-02 13:34:31 +0300

    rename vars file > data, and add getFileData and getFileType func

--- src/index.js
+++ src/index.js
@@ -1,13 +1,18 @@
+import { readFileSync } from 'node:fs';
 import path from 'node:path';
-import formatting from './formatters/index.js';
+import format from './formatters/index.js';
 import buildDiffTree from './buildDiffTree.js';
 import parse from './parsers.js';
 
+const getFileData = (file) => readFileSync(path.resolve(file), 'utf-8');
+
+const getFileType = (file) => path.extname(file).slice(1);
+
 const genDiff = (filepath1, filepath2, formatName = 'stylish') => {
-  const file1 = parse(filepath1, path.extname(filepath1));
-  const file2 = parse(filepath2, path.extname(filepath2));
-  const tree = buildDiffTree(file1, file2);
-  return formatting(tree, formatName);
+  const data1 = parse(getFileData(filepath1), getFileType(filepath1));
+  const data2 = parse(getFileData(filepath2), getFileType(filepath2));
+  const tree = buildDiffTree(data1, data2);
+  return format(tree, formatName);
 };
 
 export default genDiff;
