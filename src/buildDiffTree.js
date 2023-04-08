4bd833e76f0c14acb75f98c278270170360d3d85
Bobrikov <bobronaud@gmail.com>
2023-04-08 13:25:14 +0300

    remove buildValue(), replaced "value" to "children" in nested nodes

--- src/buildDiffTree.js
+++ src/buildDiffTree.js
@@ -1,30 +1,18 @@
 import _ from 'lodash';
 
-const buildValue = (buildedValue) => {
-  if (!_.isObject(buildedValue)) {
-    return buildedValue;
-  }
-  const entries = Object.entries(buildedValue);
-  const result = entries.reduce((acc, [key, newValue]) => {
-    const value = _.isObject(newValue) ? buildValue(newValue) : newValue;
-    return [...acc, { key, value }];
-  }, []);
-  return result;
-};
-
 const buildDiffTree = (obj1, obj2) => {
   const sortedAllKeys = _.sortBy(_.union(_.keys(obj1), _.keys(obj2)));
   return sortedAllKeys.reduce((diff, key) => {
     const value1 = obj1[key];
     const value2 = obj2[key];
-    if (_.isObject(value1) && _.isObject(value2)) {
-      return [...diff, { key, status: 'nested', value: buildDiffTree(value1, value2) }];
-    }
     if (!Object.hasOwn(obj1, key)) {
-      return [...diff, { key, status: 'added', value: buildValue(value2) }];
+      return [...diff, { key, status: 'added', value: value2 }];
     }
     if (!Object.hasOwn(obj2, key)) {
-      return [...diff, { key, status: 'removed', value: buildValue(value1) }];
+      return [...diff, { key, status: 'removed', value: value1 }];
+    }
+    if (_.isObject(value1) && _.isObject(value2)) {
+      return [...diff, { key, status: 'nested', children: buildDiffTree(value1, value2) }];
     }
     if (value1 !== value2) {
       return [
@@ -32,14 +20,12 @@ const buildDiffTree = (obj1, obj2) => {
         {
           key,
           status: 'changed',
-          value: {
-            oldValue: buildValue(value1),
-            newValue: buildValue(value2),
-          },
+          value: value2,
+          oldValue: value1,
         },
       ];
     }
-    return [...diff, { key, status: 'unchanged', value: buildValue(value1) }];
+    return [...diff, { key, status: 'unchanged', value: value1 }];
   }, []);
 };
 
